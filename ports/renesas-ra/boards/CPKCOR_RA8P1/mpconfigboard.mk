018be22486cad533d48c52f63e235e9f6af625a6
ChanghowLi <AkieDlans@outlook.com>
2026-05-15 16:16:22 +0800

    add CPKCOR_RA8P1 mpconfigboard.h and .mk

--- ports/renesas-ra/boards/CPKCOR_RA8P1/mpconfigboard.mk
+++ ports/renesas-ra/boards/CPKCOR_RA8P1/mpconfigboard.mk
@@ -5,4 +5,10 @@ LD_FILES = boards/CPKCOR_RA8P1/e2studio_gcc_freertos/Debug/fsp_gen.ld
 # MicroPython settings
 MICROPY_VFS_FAT = 1
 
-CFLAGS+=-DDEFAULT_DBG_CH=0
+CFLAGS += -DDEFAULT_DBG_CH=0
+
+# Using e2 studio generated FSP
+HAL_DIR = ports/renesas-ra/boards/CPKCOR_RA8P1/e2studio_gcc_freertos/fsp/ra
+CMSIS_DIR = ports/renesas-ra/boards/CPKCOR_RA8P1/e2studio_gcc_freertos/ra/arm/CMSIS_6/CMSIS
+STARTUP_FILE = ports/renesas-ra/boards/CPKCOR_RA8P1/e2studio_gcc_freertos/ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.o
+SYSTEM_FILE = ports/renesas-ra/boards/CPKCOR_RA8P1/e2studio_gcc_freertos/ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.o
