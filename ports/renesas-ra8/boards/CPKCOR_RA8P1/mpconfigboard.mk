3ec4f024991ee01f4b6c35448978d952dcd4913a
ChanghowLi <AkieDlans@outlook.com>
2026-06-29 18:17:34 +0800

    sync from Chenlin

--- ports/renesas-ra8/boards/CPKCOR_RA8P1/mpconfigboard.mk
+++ ports/renesas-ra8/boards/CPKCOR_RA8P1/mpconfigboard.mk
@@ -4,15 +4,23 @@
 CMSIS_MCU = RA8P1
 MCU_SERIES = m85
 
-# e2 studio project root (relative to port directory, which is the CWD)
-# This points to the generated FSP project
-E2STUDIO_DIR = boards/CPKCOR_RA8P1/e2studio_gcc_freertos
+# FSP project inputs used by the standalone MicroPython Makefile build.
+FSP_PROJECT_DIR = boards/CPKCOR_RA8P1/e2studio_gcc_freertos
+RA_CFG_DIR = $(FSP_PROJECT_DIR)/ra_cfg
+RA_GEN_DIR = $(FSP_PROJECT_DIR)/ra_gen
+BOARD_SRC_DIR = $(FSP_PROJECT_DIR)/src
+FSP_DIR = $(FSP_PROJECT_DIR)/ra/fsp
+FREERTOS_DIR = $(FSP_PROJECT_DIR)/ra/aws/FreeRTOS/FreeRTOS
+CMSIS_DIR = $(FSP_PROJECT_DIR)/ra/arm/CMSIS_6/CMSIS
+FSP_BOARD_DIR = $(FSP_PROJECT_DIR)/ra/board/ra8p1_cpkcor
 
-# Linker script from e2 studio (relative to port directory)
-LD_FILES = $(E2STUDIO_DIR)/Debug/fsp_gen.ld
+# Keep linker-generator inputs in the board directory.  The e2 studio script
+# and Debug directories are ignored build products and are not standalone
+# Makefile inputs.
+LD_SCRIPT_DIR = boards/CPKCOR_RA8P1/$(FSP_PROJECT_DIR)/Debug
+LD_SCRIPT = $(LD_SCRIPT_DIR)/fsp.ld
+FSP_LD_GENERATED_DIR = $(LD_SCRIPT_DIR)
+BSP_LINKER_INFO_DIR = $(FSP_LD_GENERATED_DIR)
 
 # MicroPython features
 MICROPY_VFS_FAT = 0
-
-# FSP include paths (relative to E2STUDIO_DIR)
-# These get expanded in the main Makefile
