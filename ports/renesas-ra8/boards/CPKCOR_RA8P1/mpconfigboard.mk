125c33f43abcaf3e81cc501c95ff8eda9bde94a8
ChanghowLi <AkieDlans@outlook.com>
2026-07-03 00:20:30 +0800

    Simplify Makefile, and update e2studio C/C++ config

--- ports/renesas-ra8/boards/CPKCOR_RA8P1/mpconfigboard.mk
+++ ports/renesas-ra8/boards/CPKCOR_RA8P1/mpconfigboard.mk
@@ -6,21 +6,12 @@ MCU_SERIES = m85
 
 # FSP project inputs used by the standalone MicroPython Makefile build.
 FSP_PROJECT_DIR = boards/CPKCOR_RA8P1/e2studio_gcc_freertos
-RA_CFG_DIR = $(FSP_PROJECT_DIR)/ra_cfg
-RA_GEN_DIR = $(FSP_PROJECT_DIR)/ra_gen
-BOARD_SRC_DIR = $(FSP_PROJECT_DIR)/src
-FSP_DIR = $(FSP_PROJECT_DIR)/ra/fsp
-FREERTOS_DIR = $(FSP_PROJECT_DIR)/ra/aws/FreeRTOS/FreeRTOS
-CMSIS_DIR = $(FSP_PROJECT_DIR)/ra/arm/CMSIS_6/CMSIS
-FSP_BOARD_DIR = $(FSP_PROJECT_DIR)/ra/board/ra8p1_cpkcor
 
 # Keep linker-generator inputs in the board directory.  The e2 studio script
 # and Debug directories are ignored build products and are not standalone
 # Makefile inputs.
-LD_SCRIPT_DIR = $(FSP_PROJECT_DIR)/Debug
 LD_SCRIPT = $(FSP_PROJECT_DIR)/script/fsp.ld
-FSP_LD_GENERATED_DIR = $(LD_SCRIPT_DIR)
-BSP_LINKER_INFO_DIR = $(FSP_LD_GENERATED_DIR)
 
 # MicroPython features
 MICROPY_VFS_FAT = 0
+MICROPY_ROM_TEXT_COMPRESSION = 1
